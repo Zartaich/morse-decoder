7abb76b10ad3da5e7cbe346b9e03da5fa4c6dbfe
Zartaich <zartaich@gmail.com>
2019-09-23 23:46:49 +0300

    feat:add solution of task

--- src/index.js
+++ src/index.js
@@ -1,46 +1,68 @@
 const MORSE_TABLE = {
-    '.-':     'a',
-    '-...':   'b',
-    '-.-.':   'c',
-    '-..':    'd',
-    '.':      'e',
-    '..-.':   'f',
-    '--.':    'g',
-    '....':   'h',
-    '..':     'i',
-    '.---':   'j',
-    '-.-':    'k',
-    '.-..':   'l',
-    '--':     'm',
-    '-.':     'n',
-    '---':    'o',
-    '.--.':   'p',
-    '--.-':   'q',
-    '.-.':    'r',
-    '...':    's',
-    '-':      't',
-    '..-':    'u',
-    '...-':   'v',
-    '.--':    'w',
-    '-..-':   'x',
-    '-.--':   'y',
-    '--..':   'z',
-    '.----':  '1',
-    '..---':  '2',
-    '...--':  '3',
-    '....-':  '4',
-    '.....':  '5',
-    '-....':  '6',
-    '--...':  '7',
-    '---..':  '8',
-    '----.':  '9',
-    '-----':  '0',
+  ".-": "a",
+  "-...": "b",
+  "-.-.": "c",
+  "-..": "d",
+  ".": "e",
+  "..-.": "f",
+  "--.": "g",
+  "....": "h",
+  "..": "i",
+  ".---": "j",
+  "-.-": "k",
+  ".-..": "l",
+  "--": "m",
+  "-.": "n",
+  "---": "o",
+  ".--.": "p",
+  "--.-": "q",
+  ".-.": "r",
+  "...": "s",
+  "-": "t",
+  "..-": "u",
+  "...-": "v",
+  ".--": "w",
+  "-..-": "x",
+  "-.--": "y",
+  "--..": "z",
+  ".----": "1",
+  "..---": "2",
+  "...--": "3",
+  "....-": "4",
+  ".....": "5",
+  "-....": "6",
+  "--...": "7",
+  "---..": "8",
+  "----.": "9",
+  "-----": "0"
 };
 
 function decode(expr) {
-    // write your solution here
+  const arrayOfValuesSeparatedByTen = expr.match(/.{1,10}/g);
+
+  const result = arrayOfValuesSeparatedByTen.reduce((res, letterByDecimal) => {
+    const arrayOfValuesSeparatedByTwo = letterByDecimal.match(/.{1,2}/g);
+
+    const stringOfMorzeSymbols = arrayOfValuesSeparatedByTwo
+      .map(morzeSymbol => {
+        if (morzeSymbol === "10") {
+          return ".";
+        } else if (morzeSymbol === "11") {
+          return "-";
+        }
+      })
+      .join("");
+
+    if (stringOfMorzeSymbols !== "") {
+      return res.concat(MORSE_TABLE[stringOfMorzeSymbols]);
+    } else {
+      return res.concat(" ");
+    }
+  }, "");
+
+  return result;
 }
 
 module.exports = {
-    decode
-}
+  decode
+};
